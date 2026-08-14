3ab4ddf657b0cf3bcaeb950c32bc115d8758c20d
kristianmukara94 <kristianmukara94@gmail.com>
2026-08-15 11:05:29 +1200

    Activity 2 data analysis

--- activity 2-TextFile_dataAnalysis.py
+++ activity 2-TextFile_dataAnalysis.py
@@ -1,25 +1,27 @@
-file_path = r"C:\Users\krist\Downloads\junk.txt"
-
-with open(file_path, "r") as data:
+# Create a function to open the filename junk.txt
+with open("junk.txt", "r") as data:
      content = data.readlines()
      for line in content:
-
+# Create a variable to count the lines inside the junk file
           total_lines =len(content)
     
-          
+# Print the total number of lines inside the junk file          
           print(content)
           print ("Total number of lines:", total_lines)
 data.close()     
 
-with open(file_path, "a") as writedata:
+# Create a function to open the junk file and then write data inside the txt
+with open("junk.txt", "a") as writedata:
      writedata.write("\n`text file nanalyssis`\n")
 writedata.close()
 
-with open(file_path, "r") as data2:
+# Create a function to read the junk file 
+with open("junk.txt", "r") as data2:
      convert = data2.read()
 data2.close()
 
-with open(file_path, "w") as convertdata:
+# Create a function to convert all data to lowercase
+with open("junk.txt", "w") as convertdata:
      convertdata.write(convert.lower())
 convertdata.close()
 
